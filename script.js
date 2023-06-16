93b18c1ad960905c9da0b2c080c27ef2ac786f38
Adil Ahamad <adilahamad.personally@gmail.com>
2023-06-16 17:51:06 +0530

    Navigate between elements and work with HTMLCollection.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Srcipt is running.");
+const list = document.getElementById("list");
+console.log(list);
+
+// navigate between elements
+    console.log(document.getRootNode)
+    console.log(document.body)
+    console.log(document.head)
+    console.log(list.parentElement)
+    console.log(list.previousElementSibling)
+    console.log(list.nextElementSibling)
+// NOTE: children propertie give HTMLCollection, array like
+    console.log(list.children)
+// -> for of loop is working...
+    for(child of list.children) {
+        console.log(child.hasAttribute("class"))
+    }
+    console.log(list.firstElementChild)
+    console.log(list.lastElementChild)
